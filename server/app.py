92a24d10a438579f28c7348fb0ba713a0d1c2cdb
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-05 12:03:03 +0300

    Flask-SQLAlchemy lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,47 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquakeByID(id):
+    earthquake=Earthquake.query.filter(Earthquake.id==id).first()
+    if earthquake:
+        body={
+            "id":earthquake.id,
+            "location":earthquake.location,
+            "magnitude":earthquake.magnitude,
+            "year":earthquake.year
+            }
+        status=200
 
+    else:
+        body={'message': f'earthquake {id} not found.'}
+        status=404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:min_magnitude>')
+def min_magnitude(min_magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= min_magnitude).limit(3).all()
+    if earthquakes:
+        quake_list = [{
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        } for earthquake in earthquakes]
+        body = {
+            "count": len(earthquakes),
+            "quakes": quake_list
+        }
+        status = 200
+    else:
+        body = {
+            'message': f'No earthquakes found with magnitude greater than or equal to {min_magnitude}'
+        }
+        status = 404
+
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
